f097825466c999829debd3e0ac5acbc665d023fb
Youssef_Frigui <youssef.frigui13@gmail.com>
2024-12-08 12:30:30 +0100

    Implement Annonce display in AcceuilController and update Acceuil.html template to show list of annonces

--- src/main/java/com/tekup/gestionimmobil/web/Controllers/AcceuilController.java
+++ src/main/java/com/tekup/gestionimmobil/web/Controllers/AcceuilController.java
@@ -1,10 +1,26 @@
 package com.tekup.gestionimmobil.web.Controllers;
 
+import com.tekup.gestionimmobil.business.services.AnnonceService;
+import com.tekup.gestionimmobil.dao.entities.Annonce;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AcceuilController {
+
+    @Autowired
+    private AnnonceService annonceService;
+
+    @GetMapping("/")
+    public String showAcceuil(Model model) {
+        List<Annonce> annonces = annonceService.findAll();
+        model.addAttribute("annonces", annonces);
+        return "Acceuil";
+    }
 
     @GetMapping("/acceuil")
     public String showAcceuilPage() {
